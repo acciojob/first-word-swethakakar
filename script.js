fac5104681f5d784c22bd0a38f793353d04e72e7
Swetha <swethakakar@gmail.com>
2025-01-07 23:12:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function firstWord(s) {
   // your code here
+	if(s==null || s=='')
+	{
+		console.log(s);
+	}
+	let sarr=[];
+	sarr=s.split(' ');
+	let ouput=sarr[0];
+	//console.log(ouput);
+	return ouput;
+	
+	
 }
 
 // Do not change the code below
